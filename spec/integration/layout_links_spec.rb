a851c94e92e5a929dbac732219565d7d506952eb
John Hinnegan <john.hinnegan@gmail.com>
2010-11-02 19:58:39 -0400

    improved tests with webrat

--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe "Layout links" do
+  def check_page_links(page)                                                                                                                                                                              
+    visit page
+    check_link("About", 'pages/about')
+    check_link("Help", 'pages/help')
+    check_link("Contact", 'pages/contact')
+    check_link("Home", 'pages/home')
+  end 
+
+  def check_link(link, template)
+    click_link link
+    response.should render_template(template)
+  end 
+
   it "should have a Home page at '/'" do
     get '/'
     response.should render_template('pages/home')
@@ -25,4 +38,23 @@ describe "Layout links" do
     get '/signup' 
     response.should render_template('users/new')
   end 
+
+  it "should have the right links on the layout" do                                                                                                                                                       
+    check_page_links(about_path)
+  end
+
+  it "should have the right links on the layout" do
+    check_page_links(help_path)
+  end
+
+  it "should have the right links on the layout" do
+    check_page_links(contact_path)
+  end
+
+  it "should have the right links on the layout" do
+    check_page_links(root_path)
+    check_link("Sign up", 'users/new')
+  end
+
 end
+
